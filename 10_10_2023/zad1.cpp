fcdd49bb187adc1890487b368434987558e6b707
ziajkajajka <145669571+ziajkajajka@users.noreply.github.com>
2023-10-10 14:58:05 +0200

    Add files via upload
    zadanie 1 i 2 zrobione

---
+++ 10_10_2023/zad1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string login;
+    string haslo;
+
+    do {
+        cout << "Wpisz login" << endl;
+        cin >> login;
+        cout << "Wpisz haslo" << endl;
+        cin >> haslo;
+
+        if (login == "admin" && haslo == "haslo") {
+            cout << "Zostales zalogowany! " << endl;
+            break;
+        }
+        else if (login == "admin1" && haslo == "haslo1") {
+            cout << "Zostales zalogowany! " << endl;
+            break;
+        }
+        else if (login == "admin2" && haslo == "haslo2") {
+            cout << "Zostales zalogowany! " << endl;
+            break;
+        }
+        else
+            cout << "Bledne dane, sprobuj ponownie! " << endl;
+    } while (true);
+
+    return 0;
+}
+
---
+++ 10_10_2023/zad2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>;
+
+using namespace std;
+
+int main()
+{
+	string haslo;
+	string login;
+	do {
+		cout << "Podaj login: " << endl;
+		cin >> login;
+		cout << "Podaj haslo: " << endl;
+		cin >> haslo;
+		if (login == "admin" && haslo == "haslo") {
+			cout << "Zostales zalogowany" << endl;
+			break;
+		}
+		else if (login == "admin1" && haslo == "haslo1") {
+			cout << "Zostales zalogowany" << endl;
+			break;
+		}
+		else if (login == "admin2" && haslo == "haslo2") {
+			cout << "Zostales zalogowany" << endl;
+			break;
+		}
+		else
+			cout << "Bledne dane! " << endl;
+			cout << "Sprobuj ponownie! " << endl;
+	} while (true);
+	return 0;
+}
