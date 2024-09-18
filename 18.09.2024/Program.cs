eac350a15cc089f9a2cc1cc13526e6c210c65d59
ziajkajajka <145669571+ziajkajajka@users.noreply.github.com>
2024-09-18 13:13:34 +0200

    Add files via upload

---
+++ 18.09.2024/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double p = 3.14;
+        Console.WriteLine(p);
+
+        string s = "Kacper Wewióra";
+        Console.WriteLine(s);
+
+        int x = 10;
+        double y = Convert.ToDouble(x);
+        Console.WriteLine(y);
+
+        double a = 10.78493;
+        int b = Convert.ToInt32(a);
+        Console.WriteLine(b);   
+
+        string napis = Console.ReadLine();
+        Console.WriteLine(napis);
+    }
+}
